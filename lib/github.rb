9d91e4e5fb7fd1dbd3de311f3609117e6cf8cc56
Brad Hubbard <brad.hubbard77@gmail.com>
2014-02-20 17:08:28 -0600

    3rd Commit of assignment. Testing phase

--- lib/github.rb
+++ lib/github.rb
@@ -1,1 +1,17 @@
+require 'unirest'
+require 'pry'
 
+module GHFeed
+  class RepoInfo
+    def self.get_repo_info(username, repo)
+      response = Unirest.get "https://api.github.com/repos/"+username+"/"+repo+"/commits", auth:{:user=>ENV['USERNAME'], :password=>ENV['password']}
+      response.body
+    end
+  end
+  class UserInfo
+    def self.gravatar_url(username)
+      response = Unirest.get "https://api.github.com/repos/"+username+"/"+repo+"/commits", auth:{:user=>ENV['USERNAME'], :password=>ENV['password']}
+      response.body
+    end
+  end
+end
